4d470c9e8c0d2c9af42abffe05da609bd33c03f8
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-07-20 19:57:30 +0800

    .

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -10,39 +10,39 @@ const BlogList = () => {
       <div className="flex justify-center gap-6 my-10">
         <button 
           onClick={() => setMenu("All")} 
-          className={menu === "All" ? `bg-black text-white py-1 px-4 rounded-sm` : ""}
+          className={menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}
         > 
           All
         </button >
         <button 
           onClick={() => setMenu("Technology")}
-          className={menu === "Technology" ? `bg-black text-white py-1 px-4 rounded-sm` : ""}
+          className={menu === "Technology" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}
         > 
           Technology
         </button>
         <button 
           onClick={() => setMenu("Startup")}
-          className={menu === "Startup" ? `bg-black text-white py-1 px-4 rounded-sm` : ""}
+          className={menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}
         > 
           Startup
         </button>
         <button 
           onClick={() => setMenu("Lifestyle")}
-          className={menu === "Lifestyle" ? `bg-black text-white py-1 px-4 rounded-sm` : ""}
+          className={menu === "Lifestyle" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}
         > 
           Lifestyle 
         </button>
       </div>
-      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 xl:mx-24">
         {blog_data
           .filter((item) => menu === "All" ? true : item.category === menu)
           .map((item, index) =>{
-            return <Blogs 
-              key={index}
+            return <Blogs
+              key={index} 
               image={item.image}
+              category={item.category}
               title={item.title}
               description={item.description}
-              category={item.category}
             />
         })}
       </div>
@@ -51,3 +51,6 @@ const BlogList = () => {
 };
 
 export default BlogList
+
+
+
